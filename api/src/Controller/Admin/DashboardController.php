db0a3a7a8ad26317e11ec110a0c59510e8623d8e
Mathye59 <lherbiermanon@gmail.com>
2025-09-26 09:57:41 +0200

    panel admin et react s'affichent enfin (vide bien sûr)

--- api/src/Controller/Admin/DashboardController.php
+++ api/src/Controller/Admin/DashboardController.php
@@ -7,12 +7,15 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-#[AdminDashboard(routePath: '/admin', routeName: 'admin')]
+#[Route('/admin', name: 'admin')]
 class DashboardController extends AbstractDashboardController
-{
+{   
+    #[Route('', name: 'admin_index')]
     public function index(): Response
     {
+        
         return parent::index();
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
