f6878717a3e4ba6c98389510b72f270c930ed53d
Mathye59 <lherbiermanon@gmail.com>
2025-10-07 21:25:34 +0200

    fixture à charger pour verifier connexion et tout le tralala et verification du flash

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -16,4 +16,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
